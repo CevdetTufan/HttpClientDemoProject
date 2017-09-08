d34ec3b97625dbec42a31d4367d40661e49abd14
CevdetTufan <tufan_022@hotmail.com>
2017-09-08 14:59:58 +0300

    Service projesi oluşturuldu.

---
+++ HttpClientDemo/HttpClientDemo.Service/Operations/IService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HttpClientDemo.Service.Operations
+{
+    public interface IService
+    {
+        string Get(string address);
+        string Post(string address);
+        string Put(string address);
+        string Delete(string address);
+    }
+}
---
+++ HttpClientDemo/HttpClientDemo.Service/Operations/ServiceOperations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace HttpClientDemo.Service.Operations
+{
+    public class ServiceOperations : IService
+    {
+        public string Host { get; set; }
+        public HttpStatusCode HttpStatusCode { get; private set; }
+        public string Token { get; set; }
+
+        public string Get(string address)
+        {
+            
+            string url = string.Format("{0}/{1}", Host, address);
+            string result = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                if (!string.IsNullOrEmpty(Token))
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", Token);
+                }
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                var response = client.GetAsync(url).Result;
+                HttpStatusCode = response.StatusCode;
+
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            return result;
+        }
+
+        public string Post(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Put(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(string address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
